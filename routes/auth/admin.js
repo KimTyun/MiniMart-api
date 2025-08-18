d731a33a73810f54b65c0079a7670b3404205b7f
K-cook517 <only05250517@gmail.com>
2025-08-18 13:21:56 +0900

    관리자 페이지의 문의 기능 생성을 위한 adminCtrl.js 파일의 기능 추가(기존 기능 수정사항 없었음)

--- routes/auth/admin.js
+++ routes/auth/admin.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getPendingSellers, approveSeller, rejectSeller, getMonth, getAllOrders } = require('../../ctrl/adminCtrl')
+const { getPendingSellers, approveSeller, rejectSeller, getMonth, getAllOrders, getQnaList, answerQna } = require('../../ctrl/adminCtrl')
 const { isAdmin } = require('../../middlewares/middlewares')
 
 router.get('/sellers/pending', isAdmin, getPendingSellers)
@@ -8,5 +8,8 @@ router.post('/sellers/approve/:id', isAdmin, approveSeller)
 router.post('/sellers/reject/:id', isAdmin, rejectSeller)
 router.get('/user/month', isAdmin, getMonth)
 router.get('/orders', isAdmin, getAllOrders)
+// 관리자 qna
+router.get('/qna', isAdmin, getQnaList)
+router.put('/qna/:qnaId/answer', isAdmin, answerQna)
 
 module.exports = router
